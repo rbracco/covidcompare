f08c25f5dd52154efba97b0eff1cbce380f00ae3
rbracco <robertbracco1@gmail.com>
2020-03-13 13:26:13 -0400

    working state level with control

--- appMap.js
+++ appMap.js
@@ -1,5 +1,5 @@
 let API_KEY_MAPBOX = 'pk.eyJ1IjoicmJyYWNjbyIsImEiOiJjazZ6Z3c1c2IwbnNkM21tdmg0eGhmeWJkIn0.IfYSBvXVOMUlmKm8zm-XZA'
-
+// let dataCovidState = getJSON('data/dataCovidState.json', data => dataCovidState = data);
 let [lat, long] = [37.8, -96]
 let zoomLevel = 5
 //'mapbox/satellite-v9'
@@ -20,8 +20,9 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: API_KEY_MAPBOX,
 }).addTo(map);
 
+let centroids = getCountyCentroids()
 
-function getCentroids(){
+function getCountyCentroids(){
     let centroids = {}
     for(let feat of countyCentroids["features"]){
         centroids[feat["properties"]["GEO_ID"]] = feat["geometry"]["coordinates"]
@@ -29,22 +30,21 @@ function getCentroids(){
     return centroids
 }
 
-let centroids = getCentroids()
-
+//get the distance in kilometers between two centroids
 function getDistance(c0, c1){
     let latDist = Math.abs(c0[0]-c1[0])
     let longDist = Math.abs(c0[1]-c1[1])
-    return Math.sqrt(latDist*latDist + longDist*longDist)
+    return Math.sqrt(latDist*latDist + longDist*longDist) * 111
 }
 
 //It will speed it up massively if I just iterate over dataCovid (where we have cases) instead of every county
-function getNeighboringRisk(geoID){
+function getNeighboringCountyRisk(geoID){
     let neighborRisk = 0.0
     for (let feat of countyData["features"]){
         let geoID2 = feat["properties"]["GEO_ID"]
         if((geoID != geoID2) & (geoID2 in dataCovid)){
-            let distance = getDistance(centroids[geoID], centroids[geoID2]) * 111 //distance in km
-            let curNeighborRisk = getRisk(feat["properties"], originalCall=false)
+            let distance = getDistance(centroids[geoID], centroids[geoID2])
+            let curNeighborRisk = getCountyRisk(feat["properties"], originalCall=false)
             if(! isNaN(curNeighborRisk)){
                 neighborRisk += curNeighborRisk/distance
             }
@@ -53,43 +53,77 @@ function getNeighboringRisk(geoID){
     return neighborRisk
 }
 
-function getRisk(props, originalCall=true){
+function getCountyRisk(props, originalCall=true){
     let pop = props.POP
     let geoID = props.GEO_ID
     pop = parseInt(pop)
-    cases = dataCovid[geoID] || 0.00001
+    cases = dataCovid[geoID] || 0
     let risk = cases/pop
     
     let neighborRisk = 0
     if(originalCall){
-        neighborRisk = getNeighboringRisk(geoID)
+        neighborRisk = getNeighboringCountyRisk(geoID)
     }
     return risk + neighborRisk
 }
 
-function getColor(props){
-    let risk = getRisk(props)
+function getCountyColor(props){
+    let risk = getCountyRisk(props)
+    return risk > 0.0001 ? '#a50f15':
+           risk > 0.00003  ? '#de2d26':
+           risk > 0.00001   ? '#fb6a4a':
+           risk > 0.000003    ? '#fc9272':
+           risk > 0.000001    ? '#fcbba1':
+           isNaN(risk)    ? '#000000':
+                         '#bbbbbb';
+}
+
+function getNeighboringStateRisk(stateID){
+    let neighborRisk = 0.0
+    for(let state of statesData.features){
+        let neighborID = state["id"]
+        let props = state["properties"]
+        if(neighborID != stateID){
+            centroidState = [dataCovidState[stateID]["LAT"], dataCovidState[stateID]["LONG"]]
+            centroidNeighbor = [dataCovidState[neighborID]["LAT"], dataCovidState[neighborID]["LONG"]]
+            let distance = getDistance(centroidState,centroidNeighbor)
+            let curNeighborRisk = getStateRisk(neighborID, props, originalCall=false)
+            if(! isNaN(curNeighborRisk)){
+                neighborRisk += curNeighborRisk/(distance/50)
+            }
+        }
+    }
+    return neighborRisk
+}
+
+function getStateRisk(stateID, props, originalCall=true){
+    let neighborRisk = 0
+    if(originalCall){
+        neighborRisk = getNeighboringStateRisk(stateID)
+    }
+    let stateRisk = dataCovidState[stateID]["CASES"]/props.population
+    return stateRisk + neighborRisk
+}
+
+function getStateColor(stateID, props){
+    let risk = getStateRisk(stateID, props)
     let pop = props.POP
     let geoID = props.GEO_ID
-    //console.log(geoID, risk)
 
     pop = parseInt(pop)
     cases = dataCovid[geoID] || 0.00001
-    //let val = cases/pop
-    let val = risk
-    //console.log("Risk", risk)
-    return val > 0.0001 ? '#a50f15':
-           val > 0.00003  ? '#de2d26':
-           val > 0.00001   ? '#fb6a4a':
-           val > 0.000003    ? '#fc9272':
-           val > 0.000001    ? '#fcbba1':
-           isNaN(val)    ? '#000000':
+    return risk > 0.0001 ? '#a50f15':
+           risk > 0.00003  ? '#de2d26':
+           risk > 0.00001   ? '#fb6a4a':
+           risk > 0.000003    ? '#fc9272':
+           risk > 0.000001    ? '#fcbba1':
+           isNaN(risk)    ? '#000000':
                          '#bbbbbb';
 }
 
 function countyStyle(feature) {
     return {
-        fillColor: getColor(feature.properties),
+        fillColor: getCountyColor(feature.properties),
         weight: 1,
         opacity: 1,
         color: 'white',
@@ -98,15 +132,21 @@ function countyStyle(feature) {
     };
 }
 
-let stateStyle = {
-    weight: 1,
-    opacity: 1,
-    color: 'black',
+function stateStyle(feature) {
+    return {
+        fillColor: getStateColor(feature.id, feature.properties),
+        weight: 1,
+        opacity: 1,
+        color: 'black',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
 }
+
 function onEachState(feature, layer) {
     layer.on({
         mouseover: highlightFeature,
-        mouseout: resetHighlight,
+        mouseout: resetHighlightState,
         click: zoomToFeature
     });
 }
@@ -121,7 +161,6 @@ function onEachCounty(feature, layer){
 
 function highlightCounty(e) {
     var layer = e.target;
-
     layer.setStyle({
         weight: 5,
         color: '#666',
@@ -138,7 +177,7 @@ function zoomToFeature(e) {
     map.fitBounds(e.target.getBounds());
 }
 
-function resetHighlight(e) {
+function resetHighlightState(e) {
     stateLayer.resetStyle(e.target);
 }
 
@@ -146,8 +185,6 @@ function resetHighlightCounty(e) {
     countyLayer.resetStyle(e.target);
 }
 
-console.log("States", statesData)
-console.log("Counties", countyData)
 
 function highlightFeature(e) {
     var layer = e.target;
@@ -170,11 +207,25 @@ var countyLayer = L.geoJson(countyData,
         style:countyStyle, 
         onEachFeature:onEachCounty
     }).addTo(map);
-// var stateLayer  = L.geoJson(statesData, 
-//     { 
-//         style:stateStyle, 
-//         onEachFeature:onEachState
-//     }).addTo(map);
+
+var stateLayer  = L.geoJson(statesData, 
+    { 
+        style:stateStyle, 
+        onEachFeature:onEachState
+    }).addTo(map);
+
+var overlayMaps = {
+    "Counties": countyLayer,
+    "States": stateLayer,
+}
+
+L.control.layers(overlayMaps).addTo(map);
+
+
+
+
+
+
 
 // // create the sidebar instance and add it to the map
 // var sidebar = L.control.sidebar({ container: 'sidebar' })
