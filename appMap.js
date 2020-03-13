8461dd44640c3ed08a1035786e016cccdb298c4b
rbracco <robertbracco1@gmail.com>
2020-03-13 13:49:11 -0400

    basic display with population data

--- appMap.js
+++ appMap.js
@@ -179,10 +179,12 @@ function zoomToFeature(e) {
 
 function resetHighlightState(e) {
     stateLayer.resetStyle(e.target);
+    info.update();
 }
 
 function resetHighlightCounty(e) {
     countyLayer.resetStyle(e.target);
+    info.update();
 }
 
 
@@ -199,6 +201,7 @@ function highlightFeature(e) {
     if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
         layer.bringToFront();
     }
+    info.update(layer.feature.properties);
 }
 
 
@@ -221,7 +224,23 @@ var overlayMaps = {
 
 L.control.layers(overlayMaps).addTo(map);
 
+var info = L.control();
 
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+// method that we will use to update the control based on feature properties passed
+info.update = function (props) {
+    console.log(props)
+    this._div.innerHTML = '<h4>US Population Density</h4>' +  (props ?
+        '<b>' + props.name + '</b><br />' + props.population + ' people'
+        : 'Hover over a state');
+};
+
+info.addTo(map);
 
 
 
