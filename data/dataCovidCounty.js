64c74c4e0f258211e0436ff25e648d6fe3c62181
rbracco <robertbracco1@gmail.com>
2020-03-14 14:26:51 -0400

    new county data with auto convert

--- data/dataCovidCounty.js
+++ data/dataCovidCounty.js
@@ -1,155 +1,475 @@
 let dataCovidCounty = {
 
-    //CALIFORNIA State ID 06
-    //Los Angeles CA
-    "0500000US06037":19,
-    //San Benito CA
-    "0500000US06069":2,
-    //San Diego CA
-    "0500000US06073":3,
-    //Humboldt CA
-    "0500000US06023":1,
-    //Sacramento CA
-    "0500000US06067":2,
-    //Placer CA
-    "0500000US06061":7,
-    //San Mateo CA
-    "0500000US06081":2,
-    //Sonoma CA
-    "0500000US06097":3,
-    //Orange CA
-    "0500000US06059":4,
-    //Contra Costa CA
-    "0500000US06013":12,
-    //San Francisco CA
-    "0500000US06075":15,
-    //Yolo CA
-    "0500000US06113":1,
-    //Santa Clara CA
-    "0500000US06085":38,
-    //Alameda CA
-    "0500000US06001":2,
-    //Madera CA
-    "0500000US06039":1,
-    //Fresno CA
-    "0500000US06019":1,
-    //Riverside CA
-    "0500000US06065":1,
-    //Shasta CA
-    "0500000US06089":1,
 
-
-    //INDIANA State ID 18
-    //Adams IN
-    "0500000US18001":1,
-    //Boone IN
-    "0500000US18011":1,
-    //Hendricks IN
-    "0500000US18063":2,
-    //Howard IN
-    "0500000US18067":1,
-    //Johnson IN
-    "0500000US18081":3,
-    //Marion IN
-    "0500000US18097":1,
-    //Noble IN
-    "0500000US18113":1,
+    //ALABAMA
+    //Elmore, Alabama
+    '0500000US01051': 1,
+    //Jefferson, Alabama
+    '0500000US01073': 1,
+    //Limestone, Alabama
+    '0500000US01083': 1,
+    //Montgomery, Alabama
+    '0500000US01101': 1,
+    //Tuscaloosa, Alabama
+    '0500000US01125': 1,
     
-
+    
+    //ARIZONA
+    //Pinal, Arizona
+    '0500000US04021': 5,
+    //Maricopa, Arizona
+    '0500000US04013': 3,
+    //Pima, Arizona
+    '0500000US04019': 1,
+    
+    
+    //CALIFORNIA
+    //Sacramento, California
+    '0500000US06067': 17,
+    //Yolo, California
+    '0500000US06113': 2,
+    //Solano, California
+    '0500000US06095': 6,
+    //Contra Costa, California
+    '0500000US06013': 25,
+    //San Francisco, California
+    '0500000US06075': 23,
+    //San Mateo, California
+    '0500000US06081': 20,
+    //Santa Clara, California
+    '0500000US06085': 79,
+    //Riverside, California
+    '0500000US06065': 12,
+    //Humboldt, California
+    '0500000US06023': 1,
+    //Shasta, California
+    '0500000US06089': 1,
+    //Placer, California
+    '0500000US06061': 8,
+    //Sonoma, California
+    '0500000US06097': 3,
+    //Marin, California
+    '0500000US06041': 3,
+    //Calaveras, California
+    '0500000US06009': 2,
+    //San Joaquin, California
+    '0500000US06077': 8,
+    //Alameda, California
+    '0500000US06001': 9,
+    //Stanislaus, California
+    '0500000US06099': 2,
+    //Madera, California
+    '0500000US06039': 1,
+    //Fresno, California
+    '0500000US06019': 1,
+    //San Benito, California
+    '0500000US06069': 3,
+    //Tulare, California
+    '0500000US06107': 1,
+    //Ventura, California
+    '0500000US06111': 2,
+    //Los Angeles, California
+    '0500000US06037': 40,
+    //Orange, California
+    '0500000US06059': 13,
+    //San Diego, California
+    '0500000US06073': 8,
+    //Imperial, California
+    '0500000US06025': 2,
+    //Santa Cruz, California
+    '0500000US06087': 7,
+    
+    
+    //COLORADO
+    //Adams, Colorado
+    '0500000US08001': 3,
+    //Arapahoe, Colorado
+    '0500000US08005': 7,
+    //Denver, Colorado
+    '0500000US08031': 15,
+    //Douglas, Colorado
+    '0500000US08035': 4,
+    //Eagle, Colorado
+    '0500000US08037': 16,
+    //El Paso, Colorado
+    '0500000US08041': 2,
+    //Gunnison, Colorado
+    '0500000US08051': 4,
+    //Jefferson, Colorado
+    '0500000US08059': 7,
+    //Larimer, Colorado
+    '0500000US08069': 1,
+    //Pitkin, Colorado
+    '0500000US08097': 1,
+    //Pueblo, Colorado
+    '0500000US08101': 1,
+    //Summit, Colorado
+    '0500000US08117': 2,
+    //Weld, Colorado
+    '0500000US08123': 2,
+    
+    
+    //CONNECTICUT
+    //Fairfield, Connecticut
+    '0500000US09001': 8,
+    //Litchfield, Connecticut
+    '0500000US09005': 3,
+    
+    
+    //DELAWARE
+    //New Castle, Delaware
+    '0500000US10003': 4,
+    
+    
+    //FLORIDA
+    //Manatee, Florida
+    '0500000US12081': 4,
+    //Hillsborough, Florida
+    '0500000US12057': 2,
+    //Santa Rosa, Florida
+    '0500000US12113': 1,
+    //Broward, Florida
+    '0500000US12011': 19,
+    //Lee, Florida
+    '0500000US12071': 4,
+    //Charlotte, Florida
+    '0500000US12015': 1,
+    //Okaloosa, Florida
+    '0500000US12091': 1,
+    //Volusia, Florida
+    '0500000US12127': 4,
+    //Nassau, Florida
+    '0500000US12089': 1,
+    //Collier, Florida
+    '0500000US12021': 3,
+    //Pinellas, Florida
+    '0500000US12103': 2,
+    //Pasco, Florida
+    '0500000US12101': 1,
+    //Miami-Dade, Florida
+    '0500000US12086': 8,
+    //Seminole, Florida
+    '0500000US12117': 1,
+    //Palm Beach, Florida
+    '0500000US12099': 5,
+    //Sarasota, Florida
+    '0500000US12115': 1,
+    //Alachua, Florida
+    '0500000US12001': 1,
+    //Duval, Florida
+    '0500000US12031': 1,
+    //Clay, Florida
+    '0500000US12019': 1,
+    //Orange, Florida
+    '0500000US12095': 1,
+    //Osceola, Florida
+    '0500000US12097': 1,
+    
+    
+    //INDIANA
+    //LaPorte, Indiana
+    '0500000US18091': 1,
+    //St. Joseph, Indiana
+    '0500000US18141': 1,
+    //Noble, Indiana
+    '0500000US18113': 1,
+    //Wells, Indiana
+    '0500000US18179': 1,
+    //Adams, Indiana
+    '0500000US18001': 1,
+    //Howard, Indiana
+    '0500000US18067': 1,
+    //Boone, Indiana
+    '0500000US18011': 1,
+    //Hendricks, Indiana
+    '0500000US18063': 2,
+    //Marion, Indiana
+    '0500000US18097': 3,
+    //Johnson, Indiana
+    '0500000US18081': 3,
+    
     //KENTUCKY
     //Harrison KY
-    "0500000US21097":5,
+    "0500000US21097":6,
     //Jefferson KY
-    "0500000US21111":1,
+    "0500000US21111":4,
     //Fayette KY
-    "0500000US21067":2,
-
-    //MASSACHUSETTS: STATE ID 25
-    //Berkshire MA
-    "0500000US25003":7,
-    //Essex MA
-    "0500000US25009":1,
-    //Middlesex MA
-    "0500000US25017":41,
-    //Norfolk MA
-    "0500000US25021":22,
-    //Suffolk MA
-    "0500000US25025":20,
-    //Worcester MA
-    "0500000US25027":1,
-
-    //NEW YORK: STATE ID 36
-    //Westchester NY
-    "0500000US36119":121,
-    //Saratoga NY
-    "0500000US36091":2,
-    //Nassau NY
-    "0500000US36059":28,
-    //Suffolk NY
-    "0500000US36103":6,
-    //Rockland NY
-    "0500000US36087":6,
-    //Ulster NY
-    "0500000US36111":1,
-    //New York NY
-    "0500000US36061":52,
-
-    //OHIO: STATE ID 39
-    //Cuyahoga OH
-    "0500000US39035":3,
-    //Stark OH
-    "0500000US39151":1,
-
-    //TENNESSEE: STATE ID 47
-    //Shelby TN
-    "0500000US47157":1,
-    //Davidson TN
-    "0500000US47037":2,
-    //Sullivan TN
-    "0500000US47163":1,
-    //Williamson TN
-    "0500000US47187":5,
-
-    //VIRGINIA: STATE ID 51
-    //Arlington VA
-    "0500000US51013":1,
-    //Falls Church VA
-    "0500000US51610":1,
-    //Virginia Beach VA
-    "0500000US51810":2,
-    //Fairfax VA
-    "0500000US51059":1,
-    //Spotsylvania VA
-    "0500000US51177":1,
-    //Loudoun VA
-    "0500000US51107":1,
+    "0500000US21067":3,
+    //Montgomery KY
+    "0500000US21173":1,
 
 
+    //LOUISIANA
+    //Bossier, Louisiana
+    '0500000US22015': 1,
+    //Caddo, Louisiana
+    '0500000US22017': 1,
+    //Jefferson, Louisiana
+    '0500000US22051': 7,
+    //Orleans, Louisiana
+    '0500000US22071': 36,
+    //St. Charles, Louisiana
+    '0500000US22089': 1,
+    //St. John the Baptist, Louisiana
+    '0500000US22095': 1,
+    //St. Tammany, Louisiana
+    '0500000US22103': 2,
+    //Terrebonne, Louisiana
+    '0500000US22109': 2,
+    
+    
+    //MASSACHUSETTS
+    //Middlesex, Massachusetts
+    '0500000US25017': 60,
+    //Norfolk, Massachusetts
+    '0500000US25021': 24,
+    //Suffolk, Massachusetts
+    '0500000US25025': 26,
+    //Worcester, Massachusetts
+    '0500000US25027': 2,
+    //Berkshire, Massachusetts
+    '0500000US25003': 9,
+    //Essex, Massachusetts
+    '0500000US25009': 2,
+    
+    
+    //MISSISSIPPI
+    //Forrest, Mississippi
+    '0500000US28035': 3,
+    //Leflore, Mississippi
+    '0500000US28083': 1,
+    //Pearl River, Mississippi
+    '0500000US28109': 1,
+    //Copiah, Mississippi
+    '0500000US28029': 1,
+    
+    
+    //NEW YORK
+    //Westchester, New York
+    '0500000US36119': 158,
+    //Nassau, New York
+    '0500000US36059': 51,
+    //Suffolk, New York
+    '0500000US36103': 28,
+    //Rockland, New York
+    '0500000US36087': 9,
+    //Ulster, New York
+    '0500000US36111': 5,
+    //Dutchess, New York
+    '0500000US36027': 3,
+    //Orange, New York
+    '0500000US36071': 3,
+    //Saratoga, New York
+    '0500000US36091': 3,
+    //Albany, New York
+    '0500000US36001': 2,
+    //Broome, New York
+    '0500000US36007': 1,
+    //Delaware, New York
+    '0500000US36025': 1,
+    //Herkimer, New York
+    '0500000US36043': 1,
+    //Monroe, New York
+    '0500000US36055': 1,
+    //Schenectady, New York
+    '0500000US36093': 1,
+    //New York, New York
+    '0500000US36061': 154,
+    
+    
+    //NORTH CAROLINA
+    //Forsyth, North Carolina
+    '0500000US37067': 2,
+    //Mecklenburg, North Carolina
+    '0500000US37119': 1,
+    //Cabarrus, North Carolina
+    '0500000US37025': 1,
+    //Durham, North Carolina
+    '0500000US37063': 1,
+    //Chatham, North Carolina
+    '0500000US37037': 1,
+    //Wake, North Carolina
+    '0500000US37183': 11,
+    //Harnett, North Carolina
+    '0500000US37085': 1,
+    //Johnston, North Carolina
+    '0500000US37101': 1,
+    //Wayne, North Carolina
+    '0500000US37191': 1,
+    //Craven, North Carolina
+    '0500000US37049': 1,
+    //Onslow, North Carolina
+    '0500000US37133': 1,
+    //Brunswick, North Carolina
+    '0500000US37019': 1,
+    
+    
+    //PENNSYLVANIA
+    //Bucks, Pennsylvania
+    '0500000US42017': 3,
+    //Chester, Pennsylvania
+    '0500000US42029': 2,
+    //Cumberland, Pennsylvania
+    '0500000US42041': 3,
+    //Delaware, Pennsylvania
+    '0500000US42045': 6,
+    //Monroe, Pennsylvania
+    '0500000US42089': 3,
+    //Northampton, Pennsylvania
+    '0500000US42095': 1,
+    //Montgomery, Pennsylvania
+    '0500000US42091': 20,
+    //Philadelphia, Pennsylvania
+    '0500000US42101': 4,
+    //Pike, Pennsylvania
+    '0500000US42103': 1,
+    //Washington, Pennsylvania
+    '0500000US42125': 1,
+    //Wayne, Pennsylvania
+    '0500000US42127': 1,
+    
+    
+    //SOUTH DAKOTA
+    //Beadle, South Dakota
+    '0500000US46005': 1,
+    //Bon Homme, South Dakota
+    '0500000US46009': 1,
+    //Charles Mix, South Dakota
+    '0500000US46023': 1,
+    //Davison, South Dakota
+    '0500000US46035': 1,
+    //McCook, South Dakota
+    '0500000US46087': 1,
+    //Minnehaha, South Dakota
+    '0500000US46099': 3,
+    //Pennington, South Dakota
+    '0500000US46103': 1,
+    
+    
+    //TENNESSEE
+    //Davidson, Tennessee
+    '0500000US47037': 10,
+    //Hamilton, Tennessee
+    '0500000US47065': 1,
+    //Jefferson, Tennessee
+    '0500000US47089': 1,
+    //Knox, Tennessee
+    '0500000US47093': 1,
+    //Rutherford, Tennessee
+    '0500000US47149': 1,
+    //Shelby, Tennessee
+    '0500000US47157': 2,
+    //Sullivan, Tennessee
+    '0500000US47163': 1,
+    //Williamson, Tennessee
+    '0500000US47187': 9,
+    
+    
+    //TEXAS
+    //Bell, Texas
+    '0500000US48027': 1,
+    //Collin, Texas
+    '0500000US48085': 6,
+    //Dallas, Texas
+    '0500000US48113': 7,
+    //Fort Bend, Texas
+    '0500000US48157': 6,
+    //Gregg, Texas
+    '0500000US48183': 1,
+    //Harris, Texas
+    '0500000US48201': 9,
+    //Montgomery, Texas
+    '0500000US48339': 3,
+    //Smith, Texas
+    '0500000US48423': 3,
+    //Tarrant, Texas
+    '0500000US48439': 1,
+    //Travis, Texas
+    '0500000US48453': 2,
+    
+    
+    //UTAH
+    //Davis, Utah
+    '0500000US49011': 1,
+    //Salt Lake, Utah
+    '0500000US49035': 3,
+    //Summit, Utah
+    '0500000US49043': 2,
+    //Weber, Utah
+    '0500000US49057': 1,
+    
+    
+    //VIRGINIA
+    //James City, Virginia
+    '0500000US51095': 7,
+    //Fairfax, Virginia
+    '0500000US51600': 6,
+    //Arlington, Virginia
+    '0500000US51013': 5,
+    //Loudoun, Virginia
+    '0500000US51107': 3,
+    //Virginia Beach, Virginia
+    '0500000US51810': 2,
+    //Prince William, Virginia
+    '0500000US51153': 2,
+    //Spotsylvania, Virginia
+    '0500000US51177': 1,
+    //Prince Edward, Virginia
+    '0500000US51147': 1,
+    //Hanover, Virginia
+    '0500000US51085': 1,
+    //Harrisonburg, Virginia
+    '0500000US51660': 1,
+    //Alexandria, Virginia
+    '0500000US51510': 1,
+    
+    
     //WASHINGTON
-    //Clark WA
-    "0500000US53011":1,
-    //Grant WA
-    "0500000US53025":1,
-    //Island WA
-    "0500000US53029":1,
-    //Jefferson WA
-    "0500000US53031":1,
-    //King WA
-    "0500000US53033":234,
-    //Kitsap WA
-    "0500000US53035":2,
-    //Kittitas WA
-    "0500000US53037":2,
-    //Pierce WA
-    "0500000US53053":17,
-    //Skagit WA
-    "0500000US53057":1,
-    //Snohomish WA
-    "0500000US53061":68,
-    //Thurston WA
-    "0500000US53067":1,
-    //Whatcom WA
-    "0500000US53073":1,
-
-
-}
+    //Clark, Washington
+    '0500000US53011': 1,
+    //Columbia, Washington
+    '0500000US53013': 1,
+    //Grant, Washington
+    '0500000US53025': 1,
+    //Grays Harbor, Washington
+    '0500000US53027': 1,
+    //Island, Washington
+    '0500000US53029': 3,
+    //Jefferson, Washington
+    '0500000US53031': 1,
+    //King, Washington
+    '0500000US53033': 328,
+    //Kitsap, Washington
+    '0500000US53035': 3,
+    //Kittitas, Washington
+    '0500000US53037': 3,
+    //Pierce, Washington
+    '0500000US53053': 19,
+    //Skagit, Washington
+    '0500000US53057': 3,
+    //Snohomish, Washington
+    '0500000US53061': 133,
+    //Thurston, Washington
+    '0500000US53067': 1,
+    //Whatcom, Washington
+    '0500000US53073': 1,
+    //Yakima, Washington
+    '0500000US53077': 2,
+    
+    
+    //WISCONSIN
+    //Dane, Wisconsin
+    '0500000US55025': 5,
+    //Fond du Lac, Wisconsin
+    '0500000US55039': 6,
+    //Milwaukee, Wisconsin
+    '0500000US55079': 2,
+    //Pierce, Wisconsin
+    '0500000US55093': 1,
+    //Racine, Wisconsin
+    '0500000US55101': 1,
+    //Sheboygan, Wisconsin
+    '0500000US55117': 3,
+    //Waukesha, Wisconsin
+    '0500000US55133': 1,
+    }
