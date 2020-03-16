808faafdc4cac3a55a7e03adcaab47b3bc5cb4ca
rbracco <robertbracco1@gmail.com>
2020-03-16 14:05:55 -0400

    refactor to preprocess in python

--- county.js
+++ county.js
@@ -1,70 +1,9 @@
-function mergeCovidCountyData(){
-    for (let geoID of Object.keys(dataCovidCounty)){
-        let cases = dataCovidCounty[geoID] 
-        let county = getCounty(geoID)
-        county["properties"]["cases"] = cases
-    }
-}
-
 function getCounty(countyID){
     return countyData["features"].find(element => element["properties"]["GEO_ID"] == countyID)
 }
 
-function calcRiskAllCounties(){
-    for(let county of countyData["features"]){
-        calcLocalCountyRisk(county["properties"])
-    }
-    for(let county of countyData["features"]){
-        calcAllNeighborCountyRisk(county["properties"])
-    }
-}
-
-function calcLocalCountyRisk(props){
-    let pop = props.POP
-    let geoID = props.GEO_ID
-    cases = dataCovidCounty[geoID] 
-    let countyRisk = cases/pop
-    props["LOCALRISK"] = countyRisk
-    return countyRisk
-}
-
-function calcAllNeighborCountyRisk(props){
-    props["SPECIFICRISK"] = {}
-    for (let neighbor of countyData["features"]){
-        calcNeighborCountyRisk(props, neighbor["properties"])
-    }
-    props["NEIGHBORRISK"] = averageNeighborRisk(props)
-    props["TOTALRISK"] = props["LOCALRISK"] + props["NEIGHBORRISK"]
-    //do something to store/return
-}
-
-function calcNeighborCountyRisk(props, neighborProps){
-    let geoID = props["GEO_ID"]
-    let geoID2 = neighborProps["GEO_ID"]
-    let distance = getDistance(centroids[geoID], centroids[geoID2])
-    let neighborLocalRisk = neighborProps["LOCALRISK"] || 0
-    if (geoID === geoID2 || distance > 500 || neighborLocalRisk === 0){
-        return
-    }
-    props["SPECIFICRISK"][geoID2] = {
-        "distance":distance,
-        "neighborRisk":neighborLocalRisk,
-    }
-}
-
-function averageNeighborRisk(props){
-    let risks = Object.values(props["SPECIFICRISK"])
-    let numRisks = risks.length
-    let avg = 0
-    for (let risk of risks){
-        let {distance, neighborRisk} = risk
-        avg += (neighborRisk/numRisks)*(1/Math.log2(distance))
-    }
-    return avg
-}
-
 function getCountyColor(props){
-    let risk = props["TOTALRISK"]
+    let risk = props["risk"]["TOTALRISK"]
     return risk > 0.0001 ? '#a50f15':
            risk > 0.00003  ? '#de2d26':
            risk > 0.00001   ? '#fb6a4a':
