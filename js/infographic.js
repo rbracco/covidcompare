31fc5f3aec594977f90d32b37e5b147361a7fafc
rbracco <robertbracco1@gmail.com>
2020-04-05 16:50:23 -0400

    alter infographic style

--- js/infographic.js
+++ js/infographic.js
@@ -63,21 +63,22 @@ function updateInfographicGraphic(props){
     let rank_total = curLayer === "States" ? stateData["features"].length:countyData["features"].length
     document.querySelector('#cases-cases-total').innerHTML = props["cases"]
     document.querySelector('#cases-cases-pc').innerHTML = props["pc_cases"].toFixed(2)
-    document.querySelector('#cases-cases-rank').innerHTML = `${props["rank_cases"]}/${rank_total}`
+    document.querySelector('#cases-cases-rank').innerHTML = `(${props["rank_cases"]}/${rank_total})`
     document.querySelector('#cases-cases-growth24h').innerHTML = displayPct(props["growth_cases24hr"])
     document.querySelector('#cases-cases-growth72h').innerHTML = displayPct(props["growth_cases72hr"])
     document.querySelector('#cases-cases-growth1w').innerHTML = displayPct(props["growth_cases1w"])
 
     document.querySelector('#deaths-deaths-total').innerHTML = props["deaths"]
     document.querySelector('#deaths-deaths-pc').innerHTML = props["pc_deaths"].toFixed(2)
-    document.querySelector('#deaths-deaths-rank').innerHTML = `${props["rank_deaths"]}/${rank_total}`
+    document.querySelector('#deaths-deaths-rank').innerHTML = `(${props["rank_deaths"]}/${rank_total})`
     document.querySelector('#deaths-deaths-growth24h').innerHTML = displayPct(props["growth_deaths24hr"])
     document.querySelector('#deaths-deaths-growth72h').innerHTML = displayPct(props["growth_deaths72hr"])
     document.querySelector('#deaths-deaths-growth1w').innerHTML = displayPct(props["growth_deaths1w"])
 
     document.querySelector('#tests-tests-total').innerHTML = props["test_total"] || "N/A"
     document.querySelector('#tests-tests-pc').innerHTML = curLayer==="States"?props["pc_tests"].toFixed(2):"N/A"
-    document.querySelector('#tests-tests-rank').innerHTML = curLayer ==="States"?`${props["rank_tests"]}/${rank_total}`:"N/A"
+    document.querySelector('#tests-tests-rank').innerHTML = curLayer ==="States"?`(${props["rank_tests"]}/${rank_total})`:"N/A"
     document.querySelector('#tests-tests-positive').innerHTML = props["test_positive"] || "N/A"
     document.querySelector('#tests-tests-negative').innerHTML = props["test_negative"] || "N/A"
+    document.querySelector('#tests-tests-grade').innerHTML = props["test_grade"] || "N/A"
 }
